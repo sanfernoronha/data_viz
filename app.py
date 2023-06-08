058f4fc2692eef02e0e5a133a02f1c983fea01be
Sanfer Noronha <40211471+sanfernoronha@users.noreply.github.com>
2023-06-08 13:54:53 -0700

    Update app.py

--- app.py
+++ app.py
@@ -1,3 +1,18 @@
 import streamlit as st
+import pickle
 
 st.title("CalRecycle Data Vizualisation")
+
+with open('model.pkl', 'rb') as f:
+    model = pickle.load(f)
+
+with open('vectors.pkl', 'rb') as f:
+    vectorizer = pickle.load(f)
+    
+waste = st.input_text("Enter Waste: ")    
+   
+text_features = vectorizer.transform([waste])
+
+# Predict the category
+category = model.predict(text_features)[0]
+st.text(category)
